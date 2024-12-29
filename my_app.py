82122455a6cff0b70b532105ff48f3c9c499b7ec
marjona09022005 <mamatqulovam53@gmail.com>
2024-12-29 20:09:48 +0500

    create my_app.py

--- my_app.py
+++ my_app.py
@@ -4,14 +4,12 @@ import numpy as np
 
 model_path = 'model.pkl'
 with open(model_path, 'rb') as file:
-    model = pickle.load(file)
+model = pickle.load(file)
 
-bio_features = [
-   "Pregnancies","Glucose","BloodPressure","SkinThickness","Insulin","DiabetesPedigreeFunction" ,"Age", "BMI"
-]
+bio_features = ["Pregnancies","Glucose","BloodPressure","SkinThickness","Insulin","DiabetesPedigreeFunction" ,"Age", "BMI"]
 
 result_mapping = {
-   0: "Donor (Sog'lom)",
+0: "Donor (Sog'lom)",
 1: "Gepatit (Qandli diabetga uchrash)",
 2: "Fibroz (Qandli diabetning oshib ketishi)",
 3: "Sirroz (Qandli diabet pasayishi)",
@@ -28,13 +26,13 @@ sex_value = 1 if sex == "Erkak" else 0
 
 inputs = []
 for feature in bio_features:
-    value = st.number_input(f"{feature}", value=0.0, step=0.1)
-    inputs.append(value)
+value = st.number_input(f"{feature}", value=0.0, step=0.1)
+inputs.append(value)
 
 user_data = [age, sex_value] + inputs
 
 
 if st.button("Natijani ko'rish"):
-    prediction = model.predict([user_data])
-    result = result_mapping.get(prediction[0], "Aniqlab bo'lmadi")
-    st.success(f"Bashorat: {result}")
+prediction = model.predict([user_data])
+result = result_mapping.get(prediction[0], "Aniqlab bo'lmadi")
+st.success(f"Bashorat: {result}")

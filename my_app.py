b18efdcab5f8235cd4ac56920166947ca107e490
marjona09022005 <mamatqulovam53@gmail.com>
2024-12-30 08:55:11 +0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pickle
 import numpy as np
 model_path = 'model.pkl'
-with open(model_path, 'rb') as file:
-     model = pickle.load(file)
+with open('model.pkl', 'wb') as file:
+ model = pickle.load(file)
 bio_features = ["Pregnancies","Glucose","BloodPressure","SkinThickness","Insulin","DiabetesPedigreeFunction" ,"Age", "BMI"]
 result_mapping = {
 0: "Donor (Sog'lom)",

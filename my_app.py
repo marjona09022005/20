8b57d735c57fa24b91d08a7a3edc9141cc8b8485
marjona09022005 <mamatqulovam53@gmail.com>
2024-12-30 09:51:36 +0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 
 model_path = 'model.pkl'
-with open('/content/model.pkl', 'rb') as file:
-       model = pickle.load(file)
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
 
 bio_features = [
     "Pregnancies","Glucose","BloodPressure","SkinThickness","Insulin","DiabetesPedigreeFunction" ,"Age", "BMI"
